f3afe81992e8959072317f3d0f8d5c2b293dcb81
fumi <bwb90382@gmail.com>
2019-11-27 05:44:14 +0900

    登録コンポーネントの入力要素を作成

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  messages: string[];
+  signupForm: FormGroup;
+  result: string;
+  isDisable: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    this.signupForm = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(''),
+      passwordConfirmed: new FormControl('')
+    });
+    this.result = "";
+    this.isDisable = true;
+    this.messages = [];
+  }
+
+  signUp(){
+    this.result = JSON.stringify(this.signupForm.value);
+  }
+
+  onCheckEmail(email: string){
+    if(email == null || email == "")
+      this.messages.push("メールアドレスは必須項目です。");
+    else if()
+      this.messages.push("メールアドレスを入力してください。");
   }
 
 }
